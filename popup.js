c358dae42fcdb61abb83afa605a3e8aff88ce9f6
Austin Thornley <austinjt1012@gmail.com>
2021-07-30 22:57:48 -0600

    First Commit!" "

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let goDarkButton = document.getElementById('goDark');
+	goDarkButton.addEventListener('click', function() {
+		console.log("hello world");
+		alert("hello world");
+	})
+})
